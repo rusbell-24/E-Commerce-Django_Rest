37ac3232bbfb7e7429563f2f84fd3ccb5468b6a7
Rusbell Ruiz Portocarrero <65029620+rusbell-24@users.noreply.github.com>
2023-10-03 20:31:04 -0500

    terminamos el repaso de serializadores

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,11 +1,62 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.decorators import  api_view #Libreria para el uso de decoradores
 from apps.users.models import User
-from apps.users.api.serializers import UserSerializer
+from apps.users.api.serializers import UserSerializer, UserListSerializer
 
-class UserAPIView(APIView):
+@api_view(['GET','POST'])       #Decorador
+def user_api_view(request):
     
-    def get(self, request):
-        users = User.objects.all()
-        users_serializer = UserSerializer(users, many=True)
-        return Response(users_serializer.data)
+    #List
+    if request.method == 'GET':
+        
+        #queryset
+        users = User.objects.all().values('id','username','email', 'password')
+        users_serializer = UserListSerializer(users, many=True)
+        
+        return Response(users_serializer.data, status=status.HTTP_200_OK)
+    
+    #Create
+    elif request.method == 'POST':
+        
+        #En este caso el serializador funciona como un validador
+        user_serializer = UserSerializer(data=request.data)
+        
+        #validation
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail_api_view(request, pk=None):
+    
+    # queryset
+    user = User.objects.filter(id = pk).first()
+
+    #Validation
+    if user:
+        
+        #retrieve
+        if request.method == 'GET':
+            user_serializer = UserSerializer(user)
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+        
+        #update
+        elif request.method == 'PUT':
+            user_serializer = UserSerializer(user, data=request.data)
+            
+            #validation
+            if user_serializer.is_valid():
+                user_serializer.save()
+                return Response(user_serializer.data, status=status.HTTP_200_OK)
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        #delete
+        elif request.method == 'DELETE':
+            user.delete()
+            return Response({'message':'Usuario eliminado correctamente!'}, status=status.HTTP_200_OK)
+        
+    return Response({'message':'No se ha encontrado un usuario con estos datos'}, status=status.HTTP_400_BAD_REQUEST)
--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -6,3 +6,29 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'  # Muestra todos los campos
         #fields = ['name', 'last_name', 'email']  -> para darle campos especificos
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password']) # Metodo para encriptar password
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        update_user = super().update(instance, validated_data) # Actualiza la informacion de la forma por defecto que tiene django rest
+        update_user.set_password(validated_data['password'])
+        update_user.save()
+        return update_user
+    
+class UserListSerializer(serializers.ModelSerializer): 
+    
+    class Meta:
+        model: User 
+        
+    def to_representation(self, instance):
+        return {
+            "id": instance["id"],
+            "username":instance["username"],
+            "email": instance["email"],
+            "password": instance["password"],
+            
+        }
